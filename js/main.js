c5d1a21df1daa1c6916cdde9b74bc8481697ec77
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-06-10 05:31:44 +0300

    Update 2015-06-10T02:31:40.762Z

--- js/main.js
+++ js/main.js
@@ -20618,5 +20618,11 @@ $(document).ready(function() {
             }
         }
     });
+    // Directions
+    if ($(".om-btn").length) {
+        $('.om-btn').click(function(event){
+            $(this).html('Спасибо');
+        });
+    }
 
 });
