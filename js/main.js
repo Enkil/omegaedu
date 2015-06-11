cc6c6d42699fff26dfda5b13d293366564023c6d
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-06-11 05:19:11 +0300

    Update 2015-06-11T02:19:09.479Z

--- js/main.js
+++ js/main.js
@@ -22727,9 +22727,38 @@ $(document).ready(function() {
     
     $('#mixitup').mixItUp({
         controls: {
-            toggleFilterButtons: true,
+            //toggleFilterButtons: true,
             activeClass: 'om-teachers__filter-btn--pressed'
         }
+    });
+    // Show/Hide teacher full card
+    if ($('.om-teachers__full-card-close').length) {
+        // show by click on small card
+        $('.om-teachers__teacher').click(function(){
+            scrollBack = $(this);
+            $('.om-teachers__full-card').removeClass('hidden');
+            $('html, body').animate({
+                scrollTop: $(".om-teachers__full-card").offset().top
+            }, 1000);
+        });
+    
+        // hide by click on cross
+        $('.om-teachers__full-card-close').click(function(){
+            $('.om-teachers__full-card').addClass('hidden');
+            $('html, body').animate({
+                scrollTop: scrollBack.offset().top
+            }, 1000);
+        });
+    }
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > 250) $('.om-teachers__filter').addClass('om-teachers__filter--fixed');
+        else $('.om-teachers__filter').removeClass('om-teachers__filter--fixed');
+    });
+    
+    $(window).on("scroll", function() {
+        var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+        if (scrollBottom < 50) $('.om-teachers__filter').removeClass('om-teachers__filter--fixed');
+        //else $('.om-teachers__filter').addClass('om-teachers__filter--fixed');
     });
 
 });
