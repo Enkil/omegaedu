ea123d11c2f467b2ce125fd386383e225540eb23
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-06-12 21:48:29 +0300

    timetable 100%

--- build/js/main.js
+++ build/js/main.js
@@ -22732,14 +22732,34 @@ $(document).ready(function() {
         }
     });
     
-    $('#mixitup-timetable').mixItUp({
-        controls: {
-            //toggleFilterButtons: true,
-            activeClass: 'om-timetable__filter-btn--pressed'
-        },
-        layout: {
-            display: 'table-cell'
-        }
+    // Filter timetable category-human
+    
+    $('.om-timetable__filter-btn--human').click(function(event){
+        event.preventDefault();
+        $('.om-timetable__filter-btn').removeClass('om-timetable__filter-btn--pressed');
+        $(this).addClass('om-timetable__filter-btn--pressed');
+        $('.category-tech, .category-socio-science, .category-human').removeClass('invisible');
+        $('.category-tech, .category-socio-science').addClass('invisible')
+    });
+    $('.om-timetable__filter-btn--tech').click(function(event){
+        event.preventDefault();
+        $('.om-timetable__filter-btn').removeClass('om-timetable__filter-btn--pressed');
+        $(this).addClass('om-timetable__filter-btn--pressed');
+        $('.category-tech, .category-socio-science, .category-human').removeClass('invisible');
+        $('.category-human, .category-socio-science').addClass('invisible')
+    });
+    $('.om-timetable__filter-btn--socio-science').click(function(event){
+        event.preventDefault();
+        $('.om-timetable__filter-btn').removeClass('om-timetable__filter-btn--pressed');
+        $(this).addClass('om-timetable__filter-btn--pressed');
+        $('.category-tech, .category-socio-science, .category-human').removeClass('invisible');
+        $('.category-tech, .category-human').addClass('invisible')
+    });
+    $('.om-timetable__filter-btn--all').click(function(event){
+        event.preventDefault();
+        $('.om-timetable__filter-btn').removeClass('om-timetable__filter-btn--pressed');
+        $(this).addClass('om-timetable__filter-btn--pressed');
+        $('.category-tech, .category-socio-science, .category-human').removeClass('invisible')
     });
     // Show/Hide teacher full card
     if ($('.om-teachers__full-card-close').length) {
--- src/js/pages--external/mixitup.js
+++ src/js/pages--external/mixitup.js
@@ -7,12 +7,32 @@ $('#mixitup-teachers').mixItUp({
     }
 });
 
-$('#mixitup-timetable').mixItUp({
-    controls: {
-        //toggleFilterButtons: true,
-        activeClass: 'om-timetable__filter-btn--pressed'
-    },
-    layout: {
-        display: 'table-cell'
-    }
+// Filter timetable category-human
+
+$('.om-timetable__filter-btn--human').click(function(event){
+    event.preventDefault();
+    $('.om-timetable__filter-btn').removeClass('om-timetable__filter-btn--pressed');
+    $(this).addClass('om-timetable__filter-btn--pressed');
+    $('.category-tech, .category-socio-science, .category-human').removeClass('invisible');
+    $('.category-tech, .category-socio-science').addClass('invisible')
+});
+$('.om-timetable__filter-btn--tech').click(function(event){
+    event.preventDefault();
+    $('.om-timetable__filter-btn').removeClass('om-timetable__filter-btn--pressed');
+    $(this).addClass('om-timetable__filter-btn--pressed');
+    $('.category-tech, .category-socio-science, .category-human').removeClass('invisible');
+    $('.category-human, .category-socio-science').addClass('invisible')
+});
+$('.om-timetable__filter-btn--socio-science').click(function(event){
+    event.preventDefault();
+    $('.om-timetable__filter-btn').removeClass('om-timetable__filter-btn--pressed');
+    $(this).addClass('om-timetable__filter-btn--pressed');
+    $('.category-tech, .category-socio-science, .category-human').removeClass('invisible');
+    $('.category-tech, .category-human').addClass('invisible')
+});
+$('.om-timetable__filter-btn--all').click(function(event){
+    event.preventDefault();
+    $('.om-timetable__filter-btn').removeClass('om-timetable__filter-btn--pressed');
+    $(this).addClass('om-timetable__filter-btn--pressed');
+    $('.category-tech, .category-socio-science, .category-human').removeClass('invisible')
 });
