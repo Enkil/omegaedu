980d91e2097b053b545f37339794640d2eeaeb5e
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-06-17 04:02:40 +0300

    pre-release

--- src/js/custom.js
+++ src/js/custom.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     //= pages--external/google-map.js
     //= pages--external/opacity-on-hover.js
     //= pages--external/slider.js
-    //= pages--external/thanx.js
+    // //= pages--external/thanx.js
     //= pages--external/mixitup.js
     //= pages--external/teachers-full-card.js
     //= pages--external/teachers-sidebar-fixed.js
